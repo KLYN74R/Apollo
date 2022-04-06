868bee18c8760fc9312a1b9144276cd3c62755d4
Vlad Chernenko <vladchernenko@protonmail.com>
2022-04-07 00:29:33 +0300

    Added encryption via AES-256

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,15 @@
 FROM debian
+
+#Move to own location
+WORKDIR /root/APOLLO
+
+COPY package*.json ./
+COPY . .
+
+RUN chmod 777 setup.sh build.sh antiven.js && ./setup.sh && pnpm install && npm link
+
+ENV NODE_ENV production
+
+#Traditionally use 7777 as default KLYNTAR port.You can change if you need(but change configs also to run instance)
+#Note:You can use external or in-built firewall,ACL and other network polices as you need(and if you know how to do it)
+EXPOSE 7777 8888 9999
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
+
 import {SUPPORTED_FORMATS} from '@klyntar/valardohaeris/vd.js'
 import {program} from 'commander'
-
+import readline from'readline'
+import crypto from'crypto'
 import pkg from 'inquirer'
-const { prompt } = pkg
-
 import fs from 'fs'
 
 
+const { prompt } = pkg
 
 
 global.__dirname = await import('path').then(async mod=>
@@ -37,7 +38,7 @@ let banner=fs.readFileSync(PATH_RESOLVE('banner.txt')).toString('utf-8')
 
 program
 
-    .version('v2.0.0')
+    .version('Apollo@v2.0.0 by KlyntarTeam')
     .description(banner)
     .usage('apollo [FLAGS] [COMMAND]')
 
@@ -114,6 +115,115 @@ program
             
         )
 
+
+program
+
+        .command('encrypt')
+        .alias('e')
+
+
+        .description(`Encrypts private keys via AES-256 symmetric algorithm to paste ciphertext to configs or for secured storage`)
+        
+        .action(async(_opts,_cmd)=>{
+
+            let rl = readline.createInterface({input: process.stdin,output: process.stdout})
+
+
+            //_____________________________________________________Get data to encrypt_______________________________________________________
+
+            
+            let privatekey=await new Promise(resolve=>rl.question('\nEnter your privatekey to encrypt ---> \x1b[33m',resolve)),
+                        
+            //PATH_TO=await new Promise(resolve=>rl.question('\n\x1b[0mEnter your PATH_TO to encrypt --->\x1b[33m',resolve)),
+            
+            password = await new Promise(resolve=>rl.question('\n\x1b[0mFinally,enter your \x1b[32mpassword\x1b[0m to encrypt ---> \x1b[33m',resolve))
+            
+            console.log('\x1b[0m')
+            
+            //_____________________________________________Prepare key and initialization vector_____________________________________________
+            
+            
+            let password_hash = crypto.createHash('sha256').update(password,'utf-8').digest('hex'),
+            
+            IV=Buffer.from(password_hash.slice(32),'hex')//Get second 16 bytes of SHA256 hash
+            
+            password_hash=password_hash.slice(0,32)//We need first 16 bytes
+            
+            
+            //_______________________________________________________Start encryption________________________________________________________
+            
+            
+            let cipher = crypto.createCipheriv('aes-256-cbc',password_hash,IV)
+            
+            //fs.writeFileSync(PATH_TO,cipher.update(fs.readFileSync(PATH_FROM,'utf-8'),'utf8','hex') + cipher.final('hex'))
+
+            let banner=fs.readFileSync(PATH_RESOLVE('crypt.txt')).toString('utf-8')
+            .replaceAll('@','\u001b[38;5;50m@\x1b[0m')
+            .replaceAll('O','\u001b[38;5;160m*\x1b[0m')
+            .replaceAll('o','\u001b[38;5;160m*\x1b[0m')
+
+            .replace('QWERTY',`Encrypted  -> \x1b[32m${cipher.update(privatekey,'utf8','hex') + cipher.final('hex')}\x1b[0m`)
+           
+            console.log(banner)
+
+            rl.close()
+
+        
+        })
+
+
+program
+
+        .command('decrypt')
+        .alias('d')
+
+
+        .description(`Decrypt private keys via AES-256 symmetric algorithm. Be careful!`)
+        
+        .action(async(_opts,_cmd)=>{
+
+            let rl = readline.createInterface({input: process.stdin,output: process.stdout}),
+
+                encryptedPrivateKey=await new Promise(resolve=>rl.question('\n\x1b[0mEnter encrypted privatekey ---> \x1b[33m',resolve)),
+
+                password=await new Promise(resolve=>rl.question('\n\x1b[0mEnter password to decrypt ---> \x1b[33m',resolve))
+    
+
+            
+            
+            
+            //Get 32 bytes SHA256(Password)
+            password=crypto.createHash('sha256').update(password,'utf-8').digest('hex')
+            
+            let IV=Buffer.from(password.slice(32),'hex')//Get second 16 bytes
+            
+            console.log('\x1b[0m')
+            
+            password=password.slice(0,32)//Retrieve first 16 bytes from hash
+            
+            
+            //________________________________________________________Decrypt values_________________________________________________________
+            
+            try{
+            
+                let decipher = crypto.createDecipheriv('aes-256-cbc',password,IV)
+            
+                let banner=fs.readFileSync(PATH_RESOLVE('crypt.txt')).toString('utf-8')
+                    .replaceAll('@','\u001b[38;5;50m@\x1b[0m')
+                    .replaceAll('O','\u001b[38;5;160m*\x1b[0m')
+                    .replaceAll('o','\u001b[38;5;160m*\x1b[0m')
+    
+                    .replace('QWERTY',`Decrypted  -> \x1b[32m${decipher.update(encryptedPrivateKey,'hex','utf8')+decipher.final('utf8')}\x1b[0m`)
+               
+                console.log(banner)
+    
+
+            }catch{ console.log('Failed') }
+            
+            rl.close()
+
+        
+        })
 
 
 program
