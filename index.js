3dc2094175294a4d2d83a462015d0f615b23679f
Vlad Chernenko <vladchernenko@protonmail.com>
2022-04-11 05:12:34 +0300

    Prepare to add addons build scripts

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 import {SUPPORTED_FORMATS} from '@klyntar/valardohaeris/vd.js'
 import {program} from 'commander'
-import readline from'readline'
 import crypto from'crypto'
 import pkg from 'inquirer'
 import fs from 'fs'
@@ -25,7 +24,7 @@ let PATH_RESOLVE=path=>__dirname+'/'+path
 
 
 
-let banner=fs.readFileSync(PATH_RESOLVE('banner.txt')).toString('utf-8')
+let banner=fs.readFileSync(PATH_RESOLVE('images/banner.txt')).toString('utf-8')
             .replaceAll('█','\u001b[38;5;50m█\x1b[0m')
             .replaceAll('*','\u001b[38;5;50m*\x1b[0m')
             .replaceAll('Q','\u001b[38;5;196mK\x1b[0m')
@@ -37,10 +36,10 @@ let banner=fs.readFileSync(PATH_RESOLVE('banner.txt')).toString('utf-8')
 
 
 program
-
-    .version('Apollo@v2.0.0 by KlyntarTeam')
+    .name('apollo')
+    .version(fs.readFileSync(PATH_RESOLVE('images/version.txt')).toString('utf-8').replaceAll('QWERTY','Apollo@v2.0.0 by KlyntarTeam'))
     .description(banner)
-    .usage('apollo [FLAGS] [COMMAND]')
+    .usage('[FLAGS] [COMMAND]')
 
 program
 
@@ -59,7 +58,7 @@ program
         
 program
 
-        .command('list')
+        .command('listkeys')
         .alias('l')
         .description(`List all supported key foramts by ValarDohaeris`)
         .action(async(name,_cmd)=>
@@ -73,8 +72,8 @@ program
                 
 program
 
-        .command('spec')
-        .description(`Call specific functions of some crypto`)
+        .command('listspec')
+        .description(`List specific functions of some crypto(e.g. address format changes,set mnemo size etc.)`)
         .action(async(name,_cmd)=>
         
             console.log(SUPPORTED_FORMATS)
@@ -88,8 +87,10 @@ program
 
         .command('sign')
         .alias('s')
-        .option('-p, --path <value>','Path to file')
 
+        .option('-t, --type <value>','Crypto project name','klyntar')
+        .option('-p, --path <value>','Path to file with payload to sign or payload itself but with prefix P:','payload.txt')
+        
         .description(`Sign the message or content(by path)`)
         .action(async(opts,_cmd)=>
         
@@ -106,7 +107,10 @@ program
 
         .command('verify')
         .alias('v')
-        .option('-p, --path <value>','Path to file')
+
+        .option('-t, --type <value>','Crypto project name','klyntar')
+        .option('-s, --sigpath <value>','Path to signature file to verify or signature itself but with prefix S:','signature.txt')
+        .option('-p, --path <value>','Path to file with payload to verify or payload itself but with prefix P:','payload.txt')
 
         .description(`Verify signed message or content(by path)`)
         .action(async(opts,_cmd)=>
@@ -120,34 +124,22 @@ program
 
         .command('encrypt')
         .alias('e')
-
+        .option('-s, --privatekey <value>','private key to encrypt')
+        .option('-p, --password <value>','password for AES encryption')
 
         .description(`Encrypts private keys via AES-256 symmetric algorithm to paste ciphertext to configs or for secured storage`)
         
-        .action(async(_opts,_cmd)=>{
+        .action(async(opts,_cmd)=>{
 
-            let rl = readline.createInterface({input: process.stdin,output: process.stdout})
-
-
-            //_____________________________________________________Get data to encrypt_______________________________________________________
-
-            
-            let privatekey=await new Promise(resolve=>rl.question('\nEnter your privatekey to encrypt ---> \x1b[33m',resolve)),
-                        
-            //PATH_TO=await new Promise(resolve=>rl.question('\n\x1b[0mEnter your PATH_TO to encrypt --->\x1b[33m',resolve)),
-            
-            password = await new Promise(resolve=>rl.question('\n\x1b[0mFinally,enter your \x1b[32mpassword\x1b[0m to encrypt ---> \x1b[33m',resolve))
-            
-            console.log('\x1b[0m')
             
             //_____________________________________________Prepare key and initialization vector_____________________________________________
             
             
-            let password_hash = crypto.createHash('sha256').update(password,'utf-8').digest('hex'),
+            let password_hash = crypto.createHash('sha256').update(opts.password,'utf-8').digest('hex'),
             
-            IV=Buffer.from(password_hash.slice(32),'hex')//Get second 16 bytes of SHA256 hash
+                IV=Buffer.from(password_hash.slice(32),'hex')//Get second 16 bytes of SHA256 hash
             
-            password_hash=password_hash.slice(0,32)//We need first 16 bytes
+                password_hash=password_hash.slice(0,32)//We need first 16 bytes
             
             
             //_______________________________________________________Start encryption________________________________________________________
@@ -155,18 +147,7 @@ program
             
             let cipher = crypto.createCipheriv('aes-256-cbc',password_hash,IV)
             
-            //fs.writeFileSync(PATH_TO,cipher.update(fs.readFileSync(PATH_FROM,'utf-8'),'utf8','hex') + cipher.final('hex'))
-
-            let banner=fs.readFileSync(PATH_RESOLVE('crypt.txt')).toString('utf-8')
-            .replaceAll('@','\u001b[38;5;50m@\x1b[0m')
-            .replaceAll('O','\u001b[38;5;160m*\x1b[0m')
-            .replaceAll('o','\u001b[38;5;160m*\x1b[0m')
-
-            .replace('QWERTY',`Encrypted  -> \x1b[32m${cipher.update(privatekey,'utf8','hex') + cipher.final('hex')}\x1b[0m`)
-           
-            console.log(banner)
-
-            rl.close()
+            console.log(cipher.update(opts.privatekey,'utf8','hex') + cipher.final('hex'))
 
         
         })
@@ -176,51 +157,33 @@ program
 
         .command('decrypt')
         .alias('d')
+        .option('-e, --encprv <value>','Encrypted private key')
+        .option('-p, --password <value>','password for AES decryption')
 
-
-        .description(`Decrypt private keys via AES-256 symmetric algorithm. Be careful!`)
+        .description(`Decrypts private keys via AES-256 symmetric algorithm. Be careful and avoid 3rd party eyes around you!`)
         
-        .action(async(_opts,_cmd)=>{
+        .action(async(opts,_cmd)=>{
 
-            let rl = readline.createInterface({input: process.stdin,output: process.stdout}),
-
-                encryptedPrivateKey=await new Promise(resolve=>rl.question('\n\x1b[0mEnter encrypted privatekey ---> \x1b[33m',resolve)),
-
-                password=await new Promise(resolve=>rl.question('\n\x1b[0mEnter password to decrypt ---> \x1b[33m',resolve))
-    
-
+               
+            //_____________________________________________Prepare key and initialization vector_____________________________________________
             
             
+            let password_hash = crypto.createHash('sha256').update(opts.password,'utf-8').digest('hex'),
             
-            //Get 32 bytes SHA256(Password)
-            password=crypto.createHash('sha256').update(password,'utf-8').digest('hex')
+                IV=Buffer.from(password_hash.slice(32),'hex')//Get second 16 bytes of SHA256 hash
             
-            let IV=Buffer.from(password.slice(32),'hex')//Get second 16 bytes
-            
-            console.log('\x1b[0m')
-            
-            password=password.slice(0,32)//Retrieve first 16 bytes from hash
-            
+                password_hash=password_hash.slice(0,32)//We need first 16 bytes
+                       
             
             //________________________________________________________Decrypt values_________________________________________________________
             
             try{
             
-                let decipher = crypto.createDecipheriv('aes-256-cbc',password,IV)
+                let decipher = crypto.createDecipheriv('aes-256-cbc',password_hash,IV)
             
-                let banner=fs.readFileSync(PATH_RESOLVE('crypt.txt')).toString('utf-8')
-                    .replaceAll('@','\u001b[38;5;50m@\x1b[0m')
-                    .replaceAll('O','\u001b[38;5;160m*\x1b[0m')
-                    .replaceAll('o','\u001b[38;5;160m*\x1b[0m')
-    
-                    .replace('QWERTY',`Decrypted  -> \x1b[32m${decipher.update(encryptedPrivateKey,'hex','utf8')+decipher.final('utf8')}\x1b[0m`)
-               
-                console.log(banner)
-    
+                console.log(decipher.update(opts.encprv,'hex','utf8')+decipher.final('utf8'))
 
             }catch{ console.log('Failed') }
-            
-            rl.close()
 
         
         })
