38bc71fcf8cca980c65b0c5b018b07dde9260b87
Vlad Chernenko <vladchernenko@protonmail.com>
2022-06-08 22:09:37 +0300

    Added fullscreen to UI

--- KLY_Modules/init/ui/route.js
+++ KLY_Modules/init/ui/route.js
@@ -1,3 +1,5 @@
+import fs from 'fs'
+
 export default (fastify, options, next) => {
 
     fastify.get('/', (request, reply) => {
@@ -6,10 +8,24 @@ export default (fastify, options, next) => {
     
     })
 
-    fastify.get('/style', (request, reply) => {
+    fastify.get('/style/:style', (request, reply) => {
     
-        reply.view('KLY_Modules/init/ui/style.css')
+        reply.view(`KLY_Modules/init/ui/styles/${request.params.style}`)
     
+    })
+
+    fastify.get('/images/:image', (request, reply) => {
+
+        console.log(`KLY_Modules/init/ui/images/${request.params.image}`)
+
+        const buffer = fs.readFileSync(`KLY_Modules/init/ui/images/${request.params.image}`)
+        
+        console.log('Buffer is ',buffer)
+
+        reply.type('image/png') // if you don't set the content, the image would be downloaded by browser instead of viewed
+        
+        reply.send(buffer)
+
     })
 
    
