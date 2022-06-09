ba6f6663ea9cda00a23610f29d5eb995997739f6
Vlad Chernenko <vladchernenko@protonmail.com>
2022-06-09 19:02:54 +0300

    Fix full screen mode

---
+++ KLY_Modules/init/ui/routes.js
@@ -0,0 +1,75 @@
+/*
+
+██╗███╗   ██╗██╗████████╗    ██████╗  ██████╗ ██╗   ██╗████████╗███████╗██████╗ ███████╗
+██║████╗  ██║██║╚══██╔══╝    ██╔══██╗██╔═══██╗██║   ██║╚══██╔══╝██╔════╝██╔══██╗██╔════╝
+██║██╔██╗ ██║██║   ██║       ██████╔╝██║   ██║██║   ██║   ██║   █████╗  ██████╔╝███████╗
+██║██║╚██╗██║██║   ██║       ██╔══██╗██║   ██║██║   ██║   ██║   ██╔══╝  ██╔══██╗╚════██║
+██║██║ ╚████║██║   ██║       ██║  ██║╚██████╔╝╚██████╔╝   ██║   ███████╗██║  ██║███████║
+╚═╝╚═╝  ╚═══╝╚═╝   ╚═╝       ╚═╝  ╚═╝ ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝╚══════╝
+
+
+
+Hello,it's Vlad :)
+
+This it the initial,the most default version of UI interface of Apollo to control KLYNTAR infrastructure and operate your coins,tokens,services data,provide information to you about symbiotes,services and so on
+In future it will be more advanced app to give your more abilities
+
+Hope,you'll like it 😀
+
+
+*/
+
+
+
+
+
+
+import fs from 'fs'
+
+export default (fastify, options, next) => {
+
+    //_______________________________________ DEFAULT ROUTES ________________________________________
+
+
+
+    //Start page
+    fastify.get('/', (request, reply) => {
+    
+        reply.view('KLY_Modules/init/ui/templates/index.ejs',{text:'Hello,this is the entry point to control Klyntar'})
+
+        
+    })
+
+    fastify.get('/start', (request, reply) => {
+    
+        reply.view('KLY_Modules/init/ui/start.ejs',{text:'Hello,this is the entry point to control Klyntar'})
+
+        
+    })
+
+
+    //Need for tests
+    fastify.get('/old', (request, reply) => {
+    
+        reply.view('./test.ejs',{text:'Hello,this is the entry point to control Klyntar'})
+    
+    })
+
+
+
+
+
+
+    //__________________________________ STYLES, IMAGES AND SO ON __________________________________
+
+
+    fastify.get('/style/:style', (request, reply) => {
+    
+        reply.view(`KLY_Modules/init/ui/styles/${request.params.style}`)
+    
+    })
+
+   
+    next()
+
+}
