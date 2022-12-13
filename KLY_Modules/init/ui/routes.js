bbbfca4fb994aaf30dc230716f8640b3efc0eb47
Vlad Chernenko <vladchernenko@protonmail.com>
2022-12-14 00:05:48 +0200

    Minor fixes

--- KLY_Modules/init/ui/routes.js
+++ KLY_Modules/init/ui/routes.js
@@ -564,7 +564,6 @@ export default (fastify, options, next) => {
     
     })
 
-
     fastify.get('/update/:param/:value', (request, reply) => {
     
         CONFIGURATION.DEFAULT[request.params.param]=request.params.value
--- KLY_Modules/init/ui/scripts/upitems.js
+++ KLY_Modules/init/ui/scripts/upitems.js
@@ -9,8 +9,16 @@ let hexToUtf8=str=>decodeURIComponent(
 
 let settings=JSON.parse(hexToUtf8(document.getElementById('settings').content))
 
+let DARTH_VADER_PATH='https://user-images.githubusercontent.com/53381472/207451252-c931618c-2884-4e34-83de-55efde93cab2.png'
+
+let VELAS_GIRL_PATH='https://user-images.githubusercontent.com/53381472/172846349-ef66e2df-b26b-4449-8dad-61a201c59a06.png'
+
+
 
 main.setAttribute('class',settings.MODE==='light'?'cyberpunk':'cyberpunk black')
+
+//Change the image
+document.getElementById('front_image').setAttribute('src',settings.MODE==='light'?VELAS_GIRL_PATH:DARTH_VADER_PATH)
 
 
 //Change theme handler
@@ -21,9 +29,12 @@ document.getElementById('theme-id').addEventListener('click',()=>{
         updatedValue=main.getAttribute('class')==='cyberpunk'?'cyberpunk black':'cyberpunk'
 
     main.setAttribute('class',updatedValue)
+
+    //Change the image
+    document.getElementById('front_image').setAttribute('src',updatedValue==='cyberpunk black'?DARTH_VADER_PATH:VELAS_GIRL_PATH)
     
 
-    fetch(`/update/MODE/${updatedValue==='cyberpunk black'?'dark':'light'}`).catch(e=>console.log('Failed'))
+    fetch(`/update/MODE/${updatedValue==='cyberpunk black'?'dark':'light'}`).catch(_=>console.log('Failed'))
 
 
 })
